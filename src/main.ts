d0f83a2a643d3f1718c148c36956b4619570bd56
charbel abi khalil <charbelak31@gmail.com>
2024-05-01 00:07:58 +0300

    modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+async function bootstrassp() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
 
 bootstrap();
