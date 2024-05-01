8215c8dec0079eaadf761dd71e07ce5ab89e8274
charbel abi khalil <charbelak31@gmail.com>
2024-05-01 14:40:57 +0300

    modified:   src/app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService], // Ensure ConfigService is provided here if it's custom
 })
 export class AppModule {}
