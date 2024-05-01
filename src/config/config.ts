c711840a5cd6d36e17d0bbe264025273fdbd3972
charbel abi khalil <charbelak31@gmail.com>
2024-05-01 14:36:26 +0300

    modified:   src/main.ts

---
+++ src/config/config.ts
@@ -0,0 +1,15 @@
+export default () => ({
+  // database: {
+  //   connectionString: process.env.CONNECTION_STRING,
+  // },
+  server: {
+    port: process.env.PORT,
+  },
+  // secret: {
+  //   JWTsecretKey: process.env.JWT_SECRET_KEY,
+  // },
+  // axiosApiInfo: {
+  //   apiKey: process.env.AXIOS_API_KEY,
+  //   senderEmail: process.env.SENDER_EMAIL,
+  // },
+});
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+//import { DotenvConfigOptions } from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+
+//dotenv.config();
+//const PORT = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  console.log('connected to port : ' + config.get('server.port'));
+  await app.listen(config.get('server.port'));
 }
 
 bootstrap();
